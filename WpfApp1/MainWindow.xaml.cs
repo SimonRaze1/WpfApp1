ffacf9b340fa28ce363aea9be4c834b2c9d00da1
Даниил Широков <daniil.shirokov.19042006@gmail.com>
2024-05-28 12:55:12 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FinanceSystem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            contentControl.Content = new Home();
+        }
+
+        private void BtnAddIncome_Click(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = new AddIncome();
+        }
+
+        private void BtnAddExpense_Click(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = new AddExpense();
+        }
+
+        private void BtnViewReport_Click(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = new Report();
+        }
+    }
+    public partial class AddIncome : UserControl
+    {
+        public AddIncome()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddIncome_Click(object sender, RoutedEvender, RoutedEventArgs e)
+        {
+            // Логика добавления дохода
+            MessageBox.Show("Доход добавлен успешно!");
+        }
+    }
+}
